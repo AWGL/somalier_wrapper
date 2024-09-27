c35a263a035db8fe8b7d73c90960e9726b2142c0
ASendellPrice <ashley.sendell.price@gmail.com>
2024-09-27 15:26:39 +0100

    testing shlex.split for passing ancestry commands

--- bin/somalier_wrapper.py
+++ bin/somalier_wrapper.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-import argparse
-import subprocess
-import os
-import glob
-import csv
-import logging
-import pandas as pd
-
 """
 ====================================================================================================================
 Somalier wrapper
@@ -29,15 +21,43 @@ python bin/somalier_wrapper.py --sample_info_csv tso_repeated_testing_anonymised
 ====================================================================================================================
 """
 
+
+import argparse
+import subprocess
+import os
+import glob
+import csv
+import logging
+import pandas as pd
+
+
 def parse_arguments():
 	argument_parser = argparse.ArgumentParser()
-	argument_parser.add_argument("--sample_info_csv", help="requires following columns: sample_id, cram_path and (optional) patient_identifier", required=True)
-	argument_parser.add_argument("--fasta", help="Path to reference genome", required=True)
+	argument_parser.add_argument(
+		"--sample_info_csv",
+		help="requires following columns: sample_id, cram_path and (optional) patient_identifier",
+		required=True,
+	)
+	argument_parser.add_argument(
+		"--fasta", help="Path to reference genome", required=True
+	)
 	argument_parser.add_argument("--sites", help="Path to sites file", required=True)
 	argument_parser.add_argument("--prefix", help="Prefix for results", required=True)
-	argument_parser.add_argument("--patient_identifier", help="Column specifying unique patient identifier (optional)", required=False)
-	argument_parser.add_argument("--somalier_1K_directory", help="Path to directory containing .somalier files for 1K genomes", required=False)
-	argument_parser.add_argument("--somalier_1K_labels", help="Path to labels file for 1K genomes samples", required=False)
+	argument_parser.add_argument(
+		"--patient_identifier",
+		help="Column specifying unique patient identifier (optional)",
+		required=False,
+	)
+	argument_parser.add_argument(
+		"--somalier_1K_directory",
+		help="Path to directory containing .somalier files for 1K genomes",
+		required=False,
+	)
+	argument_parser.add_argument(
+		"--somalier_1K_labels",
+		help="Path to labels file for 1K genomes samples",
+		required=False,
+	)
 	args = argument_parser.parse_args()
 	return args
 
@@ -52,15 +72,15 @@ def somalier_extract(file, sites, ref):
 	logging.info(f"Output directory {output_dir} successfully created")
 
 	# Load CSV file
-	with open(file, mode='r') as csv_file:
+	with open(file, mode="r") as csv_file:
 		# Create a DictReader object
 		reader = csv.DictReader(csv_file)
 
 		# Get sample IDs and CRAM paths
 		for row in reader:
-			sample_id = row['sample_id']
-			cram_path = row['cram_path']
-			
+			sample_id = row["sample_id"]
+			cram_path = row["cram_path"]
+
 			logging.info(f"Processing sample: {sample_id} with CRAM path: {cram_path}")
 
 			# Construct command for extracting informative sites from CRAMs
@@ -71,11 +91,13 @@ def somalier_extract(file, sites, ref):
 				f"--out-dir {output_dir} "
 				f"{cram_path}"
 			)
-			
+
 			# Run command
 			try:
 				if os.path.exists(f"{output_dir}/{sample_id}.somalier"):
-					logging.info(f"Extraction successful for sample: {sample_id} (using previous extraction)")
+					logging.info(
+						f"Extraction successful for sample: {sample_id} (using previous extraction)"
+					)
 				else:
 					subprocess.run(cmd, shell=True, check=True)
 					logging.info(f"Extraction successful for sample: {sample_id}")
@@ -91,13 +113,17 @@ def somalier_relatedness(file, prefix):
 
 	# Get list of .somalier files in the specified directory
 	# If no files present return error
-	somalier_files = glob.glob("somalier_output/temp/*.somalier") ## <- update so that only looks at specified samples
+	somalier_files = glob.glob(
+		"somalier_output/temp/*.somalier"
+	)  ## <- update so that only looks at specified samples
 	if not somalier_files:
-		logging.warning("No .somalier files found in 'somalier_output/temp/' directory.")
+		logging.warning(
+			"No .somalier files found in 'somalier_output/temp/' directory."
+		)
 		return
 
 	# Join list into a single string
-	somalier_files_str = ' '.join(somalier_files)
+	somalier_files_str = " ".join(somalier_files)
 
 	# Construct command
 	cmd = (
@@ -114,39 +140,47 @@ def somalier_relatedness(file, prefix):
 		logging.error("Error occurred during somalier relate.")
 		logging.error(str(e))
 
-		
+
 def somalier_relatedness_expected(file, identifier, prefix):
 	"""
-	Run somalier relate with groups file (use when sample relationships are known 
+	Run somalier relate with groups file (use when sample relationships are known
 	e.g. multiple samples are from the same patient)
 	"""
 
 	# Load sample IDs and patient identifier from CSV file
-	df = pd.read_csv(file, usecols=['sample_id', identifier])
+	df = pd.read_csv(file, usecols=["sample_id", identifier])
 
 	# Check that the given patient identifier column is present in the sample info csv
 	if identifier not in df.columns:
-		raise ValueError(f"The identifier '{identifier}' is not present in the file '{file}'.")
+		raise ValueError(
+			f"The identifier '{identifier}' is not present in the file '{file}'."
+		)
 
 	# Group by patient identifier and join sample IDs
-	grouped = df.groupby(identifier)['sample_id'].apply(lambda x: ','.join(x)).reset_index()
+	grouped = (
+		df.groupby(identifier)["sample_id"].apply(lambda x: ",".join(x)).reset_index()
+	)
 
 	# Write to file without quotes
-	output_file = 'somalier_output/expected_relationships.csv'
-	with open(output_file, 'w') as f:
-		for item in grouped['sample_id']:
+	output_file = "somalier_output/expected_relationships.csv"
+	with open(output_file, "w") as f:
+		for item in grouped["sample_id"]:
 			print(item)
 			f.write(f"{item}\n")
 
 	# Get list of .somalier files in the specified directory
 	# If no files present return error
-	somalier_files = glob.glob("somalier_output/temp/*.somalier") ## <- update so that only looks at specified samples
+	somalier_files = glob.glob(
+		"somalier_output/temp/*.somalier"
+	)  ## <- update so that only looks at specified samples
 	if not somalier_files:
-		logging.warning("No .somalier files found in 'somalier_output/temp/' directory.")
+		logging.warning(
+			"No .somalier files found in 'somalier_output/temp/' directory."
+		)
 		return
 
 	# Join list into a single string
-	somalier_files_str = ' '.join(somalier_files)
+	somalier_files_str = " ".join(somalier_files)
 
 	# Construct command for running somalier relate
 	cmd = (
@@ -159,7 +193,9 @@ def somalier_relatedness_expected(file, identifier, prefix):
 	# Run command
 	try:
 		subprocess.run(cmd, shell=True, check=True)
-		logging.info(f"Relatedness analysis completed with groups file: somalier_output/expected_relationships.csv")
+		logging.info(
+			f"Relatedness analysis completed with groups file: somalier_output/expected_relationships.csv"
+		)
 	except subprocess.CalledProcessError as e:
 		logging.error("Error occurred during somalier relate")
 		logging.error(str(e))
@@ -167,14 +203,14 @@ def somalier_relatedness_expected(file, identifier, prefix):
 
 def somalier_ancestry(path_1K, ancestry_labels, prefix):
 
-		# Get list of .somalier files for 1K genomes
+	# Get list of .somalier files for 1K genomes
 	somalier_1K_files = glob.glob(f"{path_1K}/*.somalier")
-	somalier_1K_files = ' '.join(somalier_1K_files)
+	somalier_1K_files = " ".join(somalier_1K_files)
 
 	# Get list of .somalier files for samples
 	somalier_files = glob.glob("somalier_output/temp/*.somalier")
-	somalier_files = ' '.join(somalier_files)
-	
+	somalier_files = " ".join(somalier_files)
+
 	# Construct command
 	cmd = (
 		f"somalier ancestry "
@@ -184,9 +220,12 @@ def somalier_ancestry(path_1K, ancestry_labels, prefix):
 		f"--output-prefix somalier_output/{prefix} "
 	)
 
+	# Split the command safely into arguments using shlex
+    cmd_args = shlex.split(cmd)
+
 	# Run the command
 	try:
-		subprocess.run(cmd, shell=False, check=True)
+		subprocess.run(cmd_args, shell=False, check=True)
 		logging.info(f"Ancestry prediction completed")
 	except subprocess.CalledProcessError as e:
 		logging.error("Error occurred during somalier relate")
@@ -195,23 +234,30 @@ def somalier_ancestry(path_1K, ancestry_labels, prefix):
 
 def main():
 	# Set log format
-	logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-	
+	logging.basicConfig(
+		level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+	)
+
 	# Gather command line argumentsCalledProcessError
 	args = parse_arguments()
-	
+
 	# Extract informative sites from CRAMs
 	somalier_extract(args.sample_info_csv, args.sites, args.fasta)
-	
+
 	# Calculate pairwise relatedness between all samples
 	if args.patient_identifier:
-		somalier_relatedness_expected(args.sample_info_csv, args.patient_identifier, args.prefix)
+		somalier_relatedness_expected(
+			args.sample_info_csv, args.patient_identifier, args.prefix
+		)
 	else:
 		somalier_relatedness(args.sample_info_csv, args.prefix)
-	
+
 	# Estimate sample population ancestry
 	if args.somalier_1K_directory and args.somalier_1K_labels:
-		somalier_ancestry(args.somalier_1K_directory, args.somalier_1K_labels, args.prefix)
-	
+		somalier_ancestry(
+			args.somalier_1K_directory, args.somalier_1K_labels, args.prefix
+		)
+
+
 if __name__ == "__main__":
 	main()
